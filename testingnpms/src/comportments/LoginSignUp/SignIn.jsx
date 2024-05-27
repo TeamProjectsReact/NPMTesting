971cc7bec09c3c9375e394134ddc975419638f5b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-27 19:35:57 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- testingnpms/src/comportments/LoginSignUp/SignIn.jsx
+++ testingnpms/src/comportments/LoginSignUp/SignIn.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='bg-gray-200'>
+        <div className="md:grid grid-cols-3 gap-2">
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+        </div>
+    </div>
   )
 }
 
