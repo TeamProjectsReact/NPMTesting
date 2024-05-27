adeac03becfac526c0f8c20172af0cfc5ddd971a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-27 20:07:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- testingnpms/src/App.jsx
+++ testingnpms/src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignIn from "./comportments/LoginSignUp/SignIn";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        {/* <Route path="/" element={<SignIn />} /> */}
       </Routes>
     </BrowserRouter>
   )
