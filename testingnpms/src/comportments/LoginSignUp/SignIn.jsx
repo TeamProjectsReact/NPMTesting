2ea2072ff7de66d4e8dc62f7652214716f63d8d8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-27 19:38:49 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- testingnpms/src/comportments/LoginSignUp/SignIn.jsx
+++ testingnpms/src/comportments/LoginSignUp/SignIn.jsx
@@ -6,8 +6,8 @@ const SignIn = () => {
         <div className="md:grid grid-cols-3 gap-2">
             <div className=""></div>
             <div className="">
-                <div className="bg-white py-4 px-8 rounded shadow-md">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum eligendi sint repudiandae nemo voluptatem, nisi corporis odit veritatis officiis! Dolores unde autem assumenda? Ducimus sunt tenetur cumque inventore? Dolores, assumenda.
+                <div className="bg-white py-4 px-8 rounded shadow-md w-full">
+                    hello
                 </div>
             </div>
             <div className=""></div>
