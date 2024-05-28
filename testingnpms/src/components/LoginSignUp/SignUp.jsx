e01092cb686e9bc65e7371e0ec4cfcbb462bc157
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 19:50:06 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/components/LoginSignUp/SignUp.jsx
+++ testingnpms/src/components/LoginSignUp/SignUp.jsx
@@ -3,6 +3,20 @@ import { BsMortarboardFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+     // for login data
+    const [SignUpData, SetSignUpData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    // send data to backend using axios
+    const headleSubmit = (e) => {
+        e.preventDefault();
+
+        // login to system
+        // this will be updated in future versions
+    }
   return (
     <div className='bg-gray-200 min-h-screen py-24 px-8'>
         <div className="md:grid grid-cols-3 gap-2">
@@ -12,30 +26,34 @@ const SignUp = () => {
                     <center className='text-gray-500'>
                         {/* change the Icon According to your needs */}
                         <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
-                        <p className="pt-4 text-2xl">Welcome Back</p>
+                        <p className="pt-4 text-2xl">Welcome to</p>
                         <p className="">Your Project Name</p>
                     </center>
                     <hr className='my-2'/>
                     <div className="my-4">
                         <form onSubmit={headleSubmit}>
+                             <div className="my-2 md:mx-8">
+                                <label htmlFor="" className=''>USername : </label>
+                                <input type="text" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Username'
+                                onChange={e => SetSignUpData({...SignUpData, username:e.target.value})}/>
+                            </div>
                             <div className="my-2 md:mx-8">
                                 <label htmlFor="" className=''>Email : </label>
                                 <input type="email" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Email Address'
-                                onChange={e => SetLoginData({...LoginData, email:e.target.value})}/>
+                                onChange={e => SetSignUpData({...SignUpData, email:e.target.value})}/>
                             </div>
                             <div className="my-2 md:mx-8">
                                 <label htmlFor="" className=''>Password : </label>
                                 <input type="password" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Password' 
-                                onChange={e => SetLoginData({...LoginData, password:e.target.value})}/>
+                                onChange={e => SetSignUpData({...SignUpData, password:e.target.value})}/>
                             </div>
                             <div className="my-2 md:mx-8">
-                                <button type='submit' className='mt-8 font-semibold w-full py-4 px-8 rounded bg-blue-500 text-white shadow-md duration-500 hover:bg-blue-600'>SignIn</button>
+                                <button type='submit' className='mt-8 font-semibold w-full py-4 px-8 rounded bg-blue-500 text-white shadow-md duration-500 hover:bg-blue-600'>Sign Up</button>
                             </div>
                         </form>
-                        <Link><p className="my-2 md:mx-8 text-blue-500 font-semibold">Forget Password ? </p></Link>
                     </div>
                     <hr className='my-2'/>
-                    <p className="my-4">Don't have an Account ? <Link><span className="text-blue-500">SignUp</span></Link></p>
+                    <p className="my-4">Already have an Account ? <Link to={'/'}><span className="text-blue-500">SignUp</span></Link></p>
                 </div>  
             </div>
             <div className=""></div>
