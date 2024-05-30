44674c5a680ebe25e73c80c36c4c8ac90bea70d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-30 18:24:13 +0530

    Update EditorTest.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/components/TestinEditor/EditorTest.jsx
+++ testingnpms/src/components/TestinEditor/EditorTest.jsx
@@ -17,11 +17,11 @@ const EditorTest = () => {
   
       if (start === end) return; // No text is selected
   
-      const before = inputText.substring(0, start); // 
-      const selected = inputText.substring(start, end);
-      const after = inputText.substring(end);
+      const before = inputText.substring(0, start); // get the before string in selection start
+      const selected = inputText.substring(start, end); // get the selected text
+      const after = inputText.substring(end); // last sting of the selection
   
-      const newText = `${before}**${selected}**${after}`;
+      const newText = `${before}**${selected}**${after}`; // ** use this for bold text
       setInputText(newText);
     };
     
