51f58a775c769bef801effdd6b17e3e052652616
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-30 18:58:54 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/App.jsx
+++ testingnpms/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./components/LoginSignUp/SignIn";
 import SignUp from "./components/LoginSignUp/SignUp";
 import EditorTest from "./components/TestinEditor/EditorTest";
+import NewEditor from "./components/TestinEditor/NewEditor";
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/EditorTest" element={<EditorTest /> } />
+        <Route path="/NewEditor" element={<NewEditor />} />
       </Routes> 
     </BrowserRouter>
   )
---
+++ testingnpms/src/components/TestinEditor/NewEditor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewEditor = () => {
+  return (
+    <div>NewEditor</div>
+  )
+}
+
+export default NewEditor
