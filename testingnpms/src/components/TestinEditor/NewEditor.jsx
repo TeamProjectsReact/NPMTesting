0cb498c00bd8b49765d317536eedb86208eee574
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-30 19:03:59 +0530

    Update NewEditor.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/components/TestinEditor/NewEditor.jsx
+++ testingnpms/src/components/TestinEditor/NewEditor.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const NewEditor = () => {
+    const [inputText, setInputText] = useState('');
+    const [outputText, setOutputText] = useState('');
+    const textareaRef = useRef(null);
+
   return (
-    <div>NewEditor</div>
+        <div className="min-h-screen bg-gray-100 p-4">
+          <textarea
+            // ref={textareaRef}
+            className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
+            rows="20"
+            placeholder="Type something here..."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          ></textarea>
+          <div className="w-full  p-2 bg-white border border-gray-300 rounded-md shadow-sm">
+            <div className="whitespace-pre-wrap" dangerouslySetInnerHTML={{ __html: outputText.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>') }}></div>
+          </div>
+        </div>
   )
 }
 
