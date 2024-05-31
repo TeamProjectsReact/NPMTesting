96a2a0042ac75d99ac3538179466a9ff70e36813
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-31 06:25:14 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+require('dotenv').config(); // Load environment variables
+const fs = require('fs');
+const { parse } = require('json2csv');
+const JkMysql = require('jkmysql-easy')
+
+const path = require('path')
+
+const resourceLimits = require('worker_threads');
+const e = require('express');
+const { stat } = require('fs');
+
+
+const app = express();
+const PORT = process.env.PORT || 8081
+
+//file  upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images')
+    }, 
+    filename:(req, file, cb) => {
+      cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    }
+})
+  
+const upload = multer({
+    storage:storage
+})
+
+
+// make connection between server and mysql database
+const connection = JkMysql.ConnectToDatabase('localhost', 'root', '1234', 'db_cms')
+
+// configurate email sending 
+const transporter = JkMysql.SendEmailConfig("Gmail", process.env.EMAIL_USER, process.env.EMAIL_PASSWORD)
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public')); 
+
+// endpoints
+
+
+
+app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
