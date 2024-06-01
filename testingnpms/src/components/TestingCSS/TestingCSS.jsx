938c8db2c29a55d771b35fad8838dde9b4e1657c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-01 20:14:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/components/TestingCSS/TestingCSS.jsx
+++ testingnpms/src/components/TestingCSS/TestingCSS.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {ButtonLargeBlue, ButtonBlue, ButtonRed, ButtonGray, ButtonGreen, ButtonInfo, ButtonYellow, ButtonCustom, BasicTable, BasicTableHover} from '@jehankandy/react-jkcss'
+
 
 const TestingCSS = () => {
   return (
-    <div>TestingCSS</div>
+    <div>
+        <ButtonLargeBlue>Hi all</ButtonLargeBlue>
+    </div>
   )
 }
 
