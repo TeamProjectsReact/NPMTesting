8e8902145e9f8d8bbd1a849dc76587a3c8674a9f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 07:50:19 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/App.jsx
+++ testingnpms/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./components/LoginSignUpTW/SignIn";
 import SignUp from "./components/LoginSignUpTW/SignUp";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp /> } />
+        <Route path="/Dashboard" element={<Dashboard /> } />
       </Routes> 
     </BrowserRouter>
   )
