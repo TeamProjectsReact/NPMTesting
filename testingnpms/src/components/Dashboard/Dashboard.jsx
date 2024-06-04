a52fe324c4514a8783b765e5f844ed9a4039ac3e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 08:04:44 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/components/Dashboard/Dashboard.jsx
+++ testingnpms/src/components/Dashboard/Dashboard.jsx
@@ -12,6 +12,12 @@ const Dashboard = () => {
             <div className='bg-gray-200 py-8 px-12'>
                 <div className="bg-white rounded py-8 px-4">
                     <h1 className="text-gray-500 font-semibold text-2xl">Welcome to Dashboard</h1>
+                    <hr />
+
+                    <div className="my-4 mx-8">
+                        <p className="text-xl text-gray-500">Email : { EmailUser }</p>
+                        <p className="text-xl text-gray-500">Role: { RoleUser }</p>                        
+                    </div>
                 </div>
             </div>
         )
