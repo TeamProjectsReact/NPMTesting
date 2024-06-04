823aee9f3dd1976d5f3d96430d74f71cf0adf1f4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 06:54:53 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- testingnpms/src/App.jsx
+++ testingnpms/src/App.jsx
@@ -1,10 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignIn from "./components/LoginSignUp/SignIn";
-import SignUp from "./components/LoginSignUp/SignUp";
-import EditorTest from "./components/TestinEditor/EditorTest";
-import NewEditor from "./components/TestinEditor/NewEditor";
-import TestingPack from "./components/TestingPackege/TestingPack";
-import TestingCSS from "./components/TestingCSS/TestingCSS";
+import SignIn from "./components/LoginSignUpTW/SignIn";
+import SignUp from "./components/LoginSignUpTW/SignUp";
 
 
 export default function App() {
@@ -12,11 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/EditorTest" element={<EditorTest /> } />
-        <Route path="/NewEditor" element={<NewEditor />} />
-        <Route path="/TestingPack" element={<TestingPack />} />
-        <Route path="/TestingCSS" element={<TestingCSS />} />
+        <Route path="/SignUp" element={<SignUp /> } />
       </Routes> 
     </BrowserRouter>
   )
